d33b585b7899615b0efa0d2319aefc93938907e4
unknown <nahomkeneni4@gmail.com>
2025-03-06 13:25:58 -0800

    n

--- database/database_connection.go
+++ database/database_connection.go
@@ -1,12 +1,20 @@
-package database 
+package database
 
-func DbInstance () mongo.Client{
+import (
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func DbInstance() mongo.Client {
 	db, err := mongo.NewClient(options.Client().applyURI("MONGODB_URL"))
-    if err != nil{
-         log.Fatal(err)
+	if err != nil {
+		log.Fatal(err)
 	}
 
-	var ctx, cancel = context.WithTimeout(context.Bckground(), 10 * time.Second)
+	var ctx, cancel = context.WithTimeout(context.Bckground(), 10*time.Second)
 	defer cancel()
 	if err != nil {
 		log.Fatal(err)
@@ -18,4 +26,16 @@ func DbInstance () mongo.Client{
 	}
 
 	return db
+}
+
+var Client *mongo.Client = DbInstance()
+
+func UserData(client *mongo.Client, collectionName string) *mongo.Collection {
+	var collection *mongo.Collection = client.Database("Dana").Collection(collectionName)
+	return collection
+}
+
+func ProductData(client *mongo.Client, collectionName string) *mongo.Collection {
+	var collection *mongo.Collection = client.Database("Dana").Collection(collectionName)
+	return collection
 }
