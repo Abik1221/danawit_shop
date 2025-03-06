81508339961622846e319e839bc877bf9fa3f7e1
unknown <nahomkeneni4@gmail.com>
2025-03-06 13:19:30 -0800

    nn

--- database/database_connection.go
+++ database/database_connection.go
@@ -1,1 +1,21 @@
 package database 
+
+func DbInstance () mongo.Client{
+	db, err := mongo.NewClient(options.Client().applyURI("MONGODB_URL"))
+    if err != nil{
+         log.Fatal(err)
+	}
+
+	var ctx, cancel = context.WithTimeout(context.Bckground(), 10 * time.Second)
+	defer cancel()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = db.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return db
+}
